fab0e4875b823857deb9596c09c67e1a1ed6cd68
espython <espython85@gmail.com>
2019-10-27 19:15:17 +0200

    fix config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,22 +46,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-vscode`,
             // All options are optional. Defaults shown here.
-            options: {
-              colorTheme: 'Dark+ (default dark)', // Read on for list of included themes. Also accepts object and function forms.
-              wrapperClassName: '',   // Additional class put on 'pre' tag
-              injectStyles: true,     // Injects (minimal) additional CSS for layout and scrolling
-              extensions: [],         // Extensions to download from the marketplace to provide more languages and themes
-              languageAliases: {},    // Map of custom/unknown language codes to standard/known language codes
-              replaceColor: x => x,   // Function allowing replacement of a theme color with another. Useful for replacing hex colors with CSS variables.
-              getLineClassName: ({    // Function allowing dynamic setting of additional class names on individual lines
-                content,              //   - the string content of the line
-                index,                //   - the zero-based index of the line within the code fence
-                language,             //   - the language specified for the code fence
-                codeFenceOptions      //   - any options set on the code fence alongside the language (more on this later)
-              }) => '',
-              extensionDataDirectory: // Absolute path to the directory where extensions will be downloaded. Defaults to inside node_modules.
-                path.resolve('extensions'),
-              logLevel: 'error'       // Set to 'warn' to debug if something looks wrong
+            options: {      // Set to 'warn' to debug if something looks wrong
             }
           }
         ],
